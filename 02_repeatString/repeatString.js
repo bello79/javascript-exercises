f32b7a1bf4d867620a36cd60dc7c6ef297317831
Ariel <ang.ari.bello97@gmail.com.github.com>
2023-07-25 01:11:18 -0300

    Add repeatString.js And repeatString.spec.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, repeticiones) {
+    var mensaje = "";
+    if (repeticiones < 0){
+        return "ERROR";
+    } else {
+        for(let i = 0; i < repeticiones; i++) {
+            mensaje = mensaje + word;
+        }
+        return mensaje;
+    }
 };
 
 // Do not edit below this line
